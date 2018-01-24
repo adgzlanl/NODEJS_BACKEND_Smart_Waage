0014d5ffd600e4e176f826a111086b09d53d5996
anil <anl.adgzl2515@gmail.com>
2018-01-24 22:23:43 +0100

    anmelden,registieren und hinzufügen post request  wurden hinzugefügt und von Postmann Tools  kontroliert

--- index.js
+++ index.js
@@ -1,10 +1,142 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+var express = require('express');
+var mongoose=require('mongoose');
+var bodyParser=require('body-parser');
+var jsonParser=bodyParser.json();
+var Tabelle=require('./Models/Tabelle');
+var app = express();
+/*************************************************************************** */
+mongoose.Promise=global.Promise;
+mongoose.connect('mongodb://freeversion:freeversion@ds213688.mlab.com:13688/gewichtsdatenbank');
+mongoose.connection.once('openUri', function(){
+ console.log('Connection has been made, now make fireworks...');
+ done();
+}).on('error', function(error){
+ console.log('Connection error:', error);
+});
+/*************************************************************************** */
+app.use( bodyParser.json() );     
+app.use(bodyParser.urlencoded({ 
+extended: true
+}));
+/*************************************************************************** */
+app.set('port', (process.env.PORT || 5000));
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+app.use(express.static(__dirname + '/public'));
+
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.get('/', function(request, response) {
+  response.render('pages/index');
+});
+/**************************************************************************** */
+app.post('/anmelden',jsonParser, function(request, response) {
+  
+  var email=request.body.email;
+  var kenntwort=request.body.password;
+
+  Tabelle.findOne({Email: email, Kenntwort: kenntwort}, function(err,newTable){
+
+              if(err) {
+                console.log(err);
+                return
+              }
+              else
+              {
+                if(newTable)
+
+                {
+                  response.json({success:true,ID:newTable._id,Email:newTable.Email,Kenntwort:newTable.Kenntwort});
+                  
+                }
+
+                else
+                {
+                  response.send({ success : false, message : false });
+                }
+              }            
+
+
+          });
+
+  });
+  /******************************************************************************************************************** */
+  app.post('/registieren',jsonParser, function(request, response) {
+
+    var email=request.body.email;
+    var kenntwort=request.body.password;
+  
+          Tabelle.findOne({Email: email, Kenntwort: kenntwort}, function(err,newTable){
+  
+                if(err) {
+                  console.log(err);
+                  
+                }
+                else
+                {
+                  if(newTable)
+  
+                  {
+                    response.json({success:false,message:'NotNull'});
+                    
+                  }
+  
+                  else
+                  {
+                    var tabelle=new Tabelle();
+                    
+                      tabelle.Email=email;
+                      tabelle.Kenntwort=kenntwort;
+  
+                      tabelle.save(function (err,newTable) {
+                        
+                            if(err)
+                            {
+                              response.send({ success : false, message : err });
+                            }
+                            else
+                            {
+                              response.json({success:true,ID:newTable._id,Email:newTable.Email,Kenntwort:newTable.Kenntwort});
+                              
+                            }
+                      });
+                  }
+                }            
+            });
+   });
+  /******************************************************************************************************************** */
+  app.post('/Hinzufeugen',jsonParser, function(request, response) {
+  
+    var email=request.body.email;
+    var kenntwort=request.body.password;
+    var gereatID=request.body.deviceID;
+    var thema=request.body.topic;
+    var nahricht=request.body.message;
+  
+
+          Tabelle.findOne({Email: email, Kenntwort: kenntwort}).then(function(record){
+           
+            record.Gereatsinfo.push({GereatID:gereatID,Thema:thema,Nahricht:nahricht});
+            record.save(function (err){
+
+              if(err)
+              {
+                console.log(err);
+                response.send({ success : false, message : err });
+              }
+              else
+              {
+                console.log('Datei wurde Erfolgreich hinzugefügt');
+                response.json({success:true});
+              }
+
+            });
+        });
+  
+   });
+  /******************************************************************************************************************** */
+  app.listen(app.get('port'), function() {
+    console.log('Node app is running on port', app.get('port'));
+  });
+  /******************************************************************************************************************** */
